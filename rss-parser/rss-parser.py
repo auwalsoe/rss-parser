38eee79edca36e8b6eb3a993d75f9f81cf65cda7
auwalsoe <auwalsoe@gmail.com>
2019-07-21 14:39:26 +0200

    Implemented unicode sandwich for better handling of byte strings.

--- rss-parser/rss-parser.py
+++ rss-parser/rss-parser.py
@@ -1,16 +1,11 @@
 import urllib.request
 import xml.etree.ElementTree as ET
 
-
-#print ("result code: " + str(webUrl.getcode()))
-#data = webUrl.read()
-
-
 class rss_parser():
     
     def __init__(self,rss_url):
         self.webUrl  = urllib.request.urlopen(rss_url)
-        self.data = self.webUrl.read()
+        self.data = self.webUrl.read().decode("iso-8859-1")
         self.root = ET.fromstring(self.data)
         self.feed_items = []
         self.get_version(self.root)
@@ -29,35 +24,35 @@ class rss_parser():
 
         for element in child.iter('item'):
             feed_item = {}
-            feed_item['post_title'] = str(self.checkIfNone(element.find('title')).encode("utf-8"))
+            feed_item['post_title'] = self.checkIfNone(element.find('title'))
             feed_item['post_link'] = self.checkIfNone(element.find('link'))
             feed_item['post_comments'] = self.checkIfNone(element.find('comments'))
             feed_item['post_pubDate'] = self.checkIfNone(element.find('pubDate'))
             feed_item['post_guid'] = self.checkIfNone(element.find('guid'))
-            #tmp = self.checkIfNone(element.find('description'))
-            #print(type((tmp).encode('utf-8').strip()))
-            #print(repr(tmp.encode('utf-8').strip().decode("utf-8")))
-            #feed_item['post_description'] = str(self.checkIfNone(element.find('description')).encode("utf-8"))
+            feed_item['post_description'] = self.checkIfNone(element.find('description'))
             self.feed_items.append(feed_item)
     def checkIfNone(self, element_test):
-        if element_test is None:
-            return "empty"
+        
+        if type(element_test) == type(None) or type(element_test.text) == type(None):
+            print("empty")
+            return "empty".encode("iso-8859-1")
         else:
-            return element_test.text
+            return element_test.text.encode("iso-8859-1")
     def prettyPrint(self):
         for post in self.feed_items:
             print("-----------------------------------------")
-            print("Title: " + post['post_title'])
-            print("Published: " + post['post_pubDate'])
-            print("Link: " + post['post_link'])
-            print("guid: " + post['post_guid'])
+            print("Title: " + str(post['post_title']))
+            print("Published: " + str(post['post_pubDate']))
+            print("Link: " + str(post['post_link']))
+            print("guid: " + str(post['post_guid']))
             print(' ')
-            #print(post['post_description'])
-            print(post['post_comments'])
+            print(str(post['post_description']))
+            print((post['post_comments']))
             print(' ')
             print(' ')
 if __name__ =='__main__':
     #test = rss_parser('https://blog.acolyer.org/feed/')
-    test = rss_parser('https://www.nrk.no/ostafjells/buskerud/toppsaker.rss')
+    test = rss_parser('http://feeds.bbci.co.uk/news/technology/rss.xml')
+    #test = rss_parser('https://www.nrk.no/ostafjells/buskerud/toppsaker.rss')
     print(test.rss_version)
     test.prettyPrint()
